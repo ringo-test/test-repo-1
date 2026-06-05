5c37e21ec0ec0bec3cd5f4704f757ee8468c69b5
ringo-1011 <thinh.1011@enotion.io>
2026-06-05 15:13:29 +0700

    test

--- assets/test.ts
+++ assets/test.ts
@@ -1,3 +1,6 @@
 const a = "day la chuoi tieng viet do nha";
+const b = "day la chuoi tieng viet do nha";
+const c = "day la chuoi tieng viet do nha";
+const d = "day la chuoi tieng viet do nha";
 var B = 10;
 console.log(a);
